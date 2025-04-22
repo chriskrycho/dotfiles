6921670aba23d3e4667b8f542d4f2827d5bded2e
Chris Krycho <hello@chriskrycho.com>
2025-04-22 15:36:46 -0600

    Make install step a bit more robust

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-curl https://mise.run | sh
+dry_run=false
 
-~/.local/bin/mise --version
-~/.local/bin/mise run setup
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --dry-run|-n)
+            dry_run=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+
+if [[ ! -f ~/.local/bin/task ]]; then
+    curl https://mise.run | sh
+fi
+
+export PATH="$PATH:$HOME/.local/bin"
+
+mise --version
+if [ "$dry_run" = true ]; then
+    mise run setup --dry-run
+else
+    mise run setup
+fi
