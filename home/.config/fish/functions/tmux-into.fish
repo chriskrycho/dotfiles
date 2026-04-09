be9a551feae253a8203edc5bb8e88403d0c004f0
Chris Krycho <chris.krycho@vanta.com>
2026-04-09 14:03:59 -0600

    home: make tmux-into understand ona environment names

--- home/.config/fish/functions/tmux-into.fish
+++ home/.config/fish/functions/tmux-into.fish
@@ -1,10 +1,23 @@
 function tmux-into
     if test (count $argv) -lt 1
-        echo "Usage: tmux-into <environment-id>" >&2
+        echo "Usage: tmux-into <environment-id-or-name>" >&2
         return 1
     end
 
-    gitpod environment ssh $argv[1] --start -- \
+    set input $argv[1]
+
+    # If the input looks like a UUID, use it directly; otherwise look up by name
+    if string match -qr '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$' $input
+        set env_id $input
+    else
+        set env_id (ona environment list | tail -n +2 | awk -v name=$input '$2 == name {print $1}')
+        if test -z "$env_id"
+            echo "tmux-into: no environment found with name '$input'" >&2
+            return 1
+        end
+    end
+
+    ona environment ssh $env_id --start -- \
         -R 52698:localhost:52698 \
         -t "tmux -CC new -A -s gitpod"
 end
