05e0ae838660a9990b9ca6225fd4882175547cc2
Chris Krycho <chris.krycho@vanta.com>
2026-04-29 20:49:41 +0000

    home: make sure remote environment is always displayed

--- home/.config/fish/functions/_tide_item_context.fish
+++ home/.config/fish/functions/_tide_item_context.fish
@@ -1,16 +1,19 @@
 function _tide_item_context
-    if set -q SSH_TTY
+    set -l is_remote false
+    if set -q SSH_CONNECTION; or set -q SSH_CLIENT; or set -q SSH_TTY
+        set is_remote true
+    end
+
+    if test "$is_remote" = true
         set -fx tide_context_color $tide_context_color_ssh
     else if test "$EUID" = 0
         set -fx tide_context_color $tide_context_color_root
-    else if test "$tide_context_always_display" = true
-        set -fx tide_context_color $tide_context_color_default
     else
-        return
+        set -fx tide_context_color $tide_context_color_default
     end
 
-    # In SSH sessions, prefer the ona environment name or ID over user@host.
-    if set -q SSH_TTY; and command -sq ona
+    # Prefer the ona environment name or ID over user@host when available.
+    if command -sq ona
         set -l env_name (ona environment get --field name 2>/dev/null | string trim)
         if test -n "$env_name"
             _tide_print_item context $env_name
@@ -22,6 +25,10 @@ function _tide_item_context
             _tide_print_item context $env_id
             return
         end
+    end
+
+    if test "$is_remote" != true; and test "$EUID" != 0; and test "$tide_context_always_display" != true
+        return
     end
 
     # Fall back to user@hostname (matches built-in tide behaviour).
